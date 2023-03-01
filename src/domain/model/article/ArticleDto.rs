c16379b0c1910ea38dad8fe3a6125dc93a4562e8
otobakataro <vancyroletaka@gmail.com>
2023-03-02 00:15:05 +0900

    add aticleDto

--- src/domain/model/article/ArticleDto.rs
+++ src/domain/model/article/ArticleDto.rs
@@ -2,4 +2,11 @@
 #[derive(Debug, Serialize)]
 pub struct ArticleDto {
     /** フィールド定義 */
+    pub id: Integer,
+    pub title: String,
+    pub description: String,
+    pub genre: String,
+    pub state: String,
+    pub created_at: Datetime,
+    pub updated_at: Datetime
 }
