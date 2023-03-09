45ea63d2da227ae8ccbd4eefd1f6184c37d0e6d2
otobakataro <vancyroletaka@gmail.com>
2023-03-10 01:18:29 +0900

    fix controller

--- src/controllers/estimationController.rs
+++ src/controllers/estimationController.rs
@@ -1,6 +1,6 @@
-use actix_web::{HttpResponse, web};
+use actix_web::{ HttpResponse, HttpRequest, web };
 use serde::Deserialize;
-use crate::application::service::estimationService::EstimationService;
+use crate::application::service::estimationService::{EstimationService, self};
 
 #[derive(Deserialize)]
 pub struct EstimateRequest {
@@ -9,22 +9,12 @@ pub struct EstimateRequest {
     desired_amount: String
 }
 
-/** estimationControllerのtrait定義 */
-pub trait EstimationController {
-    fn create(&self) -> HttpResponse;
-}
-
-/** estimationControllerの構造体の定義 */
-pub struct EstimationControllerImpl<E: EstimationService> {
-    estimation_service: E,
-}
-
 /** estimationControllerの実装定義 */
-impl<E: EstimationService> EstimationController for EstimationControllerImpl<E> {
-    fn create(&self) -> HttpResponse {
-        let response = self.estimation_service.estimationRequest(estimate);
-        /* estimationRequestの登録を行う dxo -> call service layer -> response */
-        format!("estimateRquestの中身は id: {}, name: {}, amount: {}", estimate.id, estimate.name, estimate.desired_amount);
-        HttpResponse::Ok().body("json返すよ!")
-    } 
-}
+pub async fn create(req: HttpRequest, estimate: web::Json<EstimateRequest>) -> HttpResponse {
+    format!("estimationRequestの値は {:?}", req);
+    EstimationService::estimationRequest(estimate);
+    // let response = self.estimation_service.estimationRequest(estimate);
+    /* estimationRequestの登録を行う dxo -> call service layer -> response */
+    // format!("estimateRquestの中身は id: {}, name: {}, amount: {}", estimate.id, estimate.name, estimate.desired_amount);
+    HttpResponse::Ok().body("json返すよ!")
+} 
--- src/routes.rs
+++ src/routes.rs
@@ -1,11 +1,7 @@
-use std::sync::Mutex;
-
-use actix_web::{ web, HttpRequest };
+use actix_web::{ web };
 use crate::controllers::estimationController;
 
 
 pub fn estimation_request(config: &mut web::ServiceConfig) {
-    config.service(web::resource("/estimate/{code}")
-            .route(web::get().to(estimationController::index)))
-            .service(web::resource("/estimate").route(web::post().to(estimationController::EstimationController::create(&self))));
+    config.service(web::resource("/estimation").route(web::post().to(estimationController::create)));
 }
