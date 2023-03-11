b8fac96f7ca6ad271d89fca4e090a88ff6264fd2
otobakataro <vancyroletaka@gmail.com>
2023-03-11 23:56:48 +0900

    add comment

--- src/domain/domainService/EstimationRequestService.rs
+++ src/domain/domainService/EstimationRequestService.rs
@@ -1,4 +1,12 @@
 /**
  * ドメインサービス
  */
-trait EstimationRequestService {}
+trait EstimationRequestService {
+
+}
+
+/** trait　base DIでEstimationRequestRepository作成 */
+
+impl EstimationRequestService {
+    /** 特定の見積もり依頼が発行済みであるかどうかを判定する方法 */
+}
--- src/domain/model/estimationRequest/EstimationRequest.rs
+++ src/domain/model/estimationRequest/EstimationRequest.rs
@@ -12,4 +12,8 @@ impl EstimationRequest {
     fn new(id: EstimationRequestId, name: String, desired_amount: String) -> EstimationRequest {
         EstimationRequest { id, name, desired_amount }
     }
-}   
+
+    /** 別パターンのオブジェクト生成のファクトリメソッドを定義 */
+
+    /** 希望金額を非表示にするタスク　リクエストからのフラグに応じて isPublic true 公開 false 非公開 メソッド非公開 */
+}
