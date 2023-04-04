48f7b8969e980588e7b244c5e0f47fb2456189db
otobakataro <vancyroletaka@gmail.com>
2023-04-05 00:37:09 +0900

    connection pool settings

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,20 @@ use rust_rest_api::routes;
 
 use actix_web::{ App, HttpServer };
 
+mod lib;
+mod models;
+mod schema;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    let pool = db::establish_connection();
+
     HttpServer::new(|| {
         App::new()
             /** ペイロードとして送れる最大容量 */
             .app_data(web::Form::default().limit(4096))
+            .app_data(Data::new(pool.clone()))
             .configure(routes::estimation_request)
     })
     .bind(("127.0.0.1", 8080))?
